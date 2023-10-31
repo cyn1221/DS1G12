fee5d97523bdc4c737ca8a5e9ec3e0d3979910d0
freyahu2000 <freyahu2000@gmail.com>
2023-10-30 21:31:01 -0400

    1

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,5 +1,22 @@
-library(readr)
-library(tidyverse)
+#' Boxplot Visualization for Payments by DRG Code
+#'
+#' This function generates a boxplot of a specified payment variable (e.g.,
+#' "Average Medicare Payments") for different DRG codes. The visualization is
+#' powered by the ggplot2 package, offering a clear and concise representation
+#' of the distribution of payments across DRG codes.
+#'
+#' @param var The payment variable of interest. It can be "Average Medicare Payments",
+#' "Average Total Payments", "Average Covered Charges", or any other valid column name
+#' in the dataset.
+#'
+#' @return A ggplot2 boxplot showing the distribution of the selected payment variable
+#' by DRG code.
+#' @export
+#'
+#' @examples
+#' boxplot(var = "Average.Medicare.Payments")
+#' boxplot(var = "Average.Total.Payments")
+#' boxplot(var = "Average.Covered.Charges")
 
 boxplot <- function(var) {
   ## read data
@@ -7,8 +24,10 @@ boxplot <- function(var) {
   plot <- DRG_data %>%
     ggplot(aes(x=DRG.Definition,y=get(var))) +
     geom_boxplot() +
-    labs(title = paste0("Boxplot of ", var, " by DRG Code"),
-           y = paste0(var))
+    labs(title = paste0("Boxplot of ",gsub("\\.", " ", var)," by DRG Code"),
+         x = 'DRG Code',
+         y = gsub("\\.", " ", var))
+  return(plot)
 }
 
-boxplot('Average Medicare Payments')
+boxplot('Average.Medicare.Payments')
